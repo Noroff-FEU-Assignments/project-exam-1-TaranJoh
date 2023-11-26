ed8b54e55eaacd1d66bef3f6d6595dc33b7995d8
TaranJoh <tarjoh51636@stud.noroff.no>
2023-11-26 20:29:16 +0100

    added carousel to home page

--- js/index.js
+++ js/index.js
@@ -13,13 +13,9 @@ async function fetchAPI() {
     console.log(posts);
 
     for (let i = 0; i < posts.length; i++) {
-      if (i === 4) {
-        break;
-      } else {
-        latestPosts.innerHTML += `<div class="post-thumbnail"><a href="blog-post.html?id=${posts[i].id}"><img src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="Book cover for ${posts[i]._embedded["wp:term"][1][0].name}">
+      latestPosts.innerHTML += `<div class="post-thumbnail"><a href="blog-post.html?id=${posts[i].id}"><img src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="Book cover for ${posts[i]._embedded["wp:term"][1][0].name}">
                             <h3>"${posts[i].title.rendered}"</h3>
                             <p>${posts[i]._embedded["wp:term"][1][0].name}</p></div>`;
-      }
     }
   } catch {
     {
@@ -29,3 +25,30 @@ async function fetchAPI() {
 }
 
 fetchAPI();
+
+// carousel
+const initSlider = () => {
+  const reviewList = document.querySelector(".carousel");
+  const slideButtons = document.querySelectorAll(".slide-button");
+  const maxScrollLeft = reviewList.scrollWidth - reviewList.clientWidth;
+
+  // slide through posts according to the slide button clicks
+  slideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev-slide" ? -1 : 1;
+      const scrollAmount = reviewList.clientWidth * direction;
+      reviewList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+  });
+
+  const handleSlideButtons = () => {
+    slideButtons[0].style.display = reviewList.scrollLeft <= 0 ? "none" : "block";
+    // slideButtons[1].style.display = reviewList.scrollLeft >= maxScrollLeft ? "none" : "block";
+  };
+
+  reviewList.addEventListener("scroll", () => {
+    handleSlideButtons();
+  });
+};
+
+window.addEventListener("load", initSlider);
