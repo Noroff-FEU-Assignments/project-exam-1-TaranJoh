63f4fcd7a502f275fcddd719548988905cce98f4
TaranJoh <tarjoh51636@stud.noroff.no>
2023-11-28 12:25:54 +0100

    updates to responsiveness

--- js/blog-list.js
+++ js/blog-list.js
@@ -13,9 +13,13 @@ async function fetchAPI() {
     console.log(posts);
 
     for (let i = 0; i < posts.length; i++) {
-      bookReviews.innerHTML += `<div class="blog-list-post"><a href="blog-post.html?id=${posts[i].id}"><img src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="Book cover for ${posts[i]._embedded["wp:term"][1][0].name}">
+      if (i === 12) {
+        break;
+      } else {
+        bookReviews.innerHTML += `<div class="blog-list-post"><a href="blog-post.html?id=${posts[i].id}"><img src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="Book cover for ${posts[i]._embedded["wp:term"][1][0].name}">
                             <h3>"${posts[i].title.rendered}"</h3>
                             <p class="title-author">${posts[i]._embedded["wp:term"][1][0].name} by ${posts[i]._embedded["wp:term"][0][0].name}</p></div>`;
+      }
     }
   } catch {
     {
